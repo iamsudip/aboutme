8cddeced9553a60caf7e45bf6ad3e6041e7cf535
iamsudip <iamsudip@programmer.net>
2014-08-24 15:07:15 +0530

    Added requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(
+    name='aboutme',
+    version='1.0',
+    description='This is my own about.me application to show my resume',
+    author='iamsudip',
+    author_email='iamsudip@programmer.net',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=[
+        'Flask==0.10.1',
+        'Flask-SQLAlchemy==1.0',
+        'Flask-Login==0.2.7',
+        'Flask-WTF==0.9.2'
+    ],
+)
