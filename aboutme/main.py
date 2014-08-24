59e924a25af2a86bae573442d3ade0e111ee41f3
iamsudip <iamsudip@programmer.net>
2014-08-24 19:32:01 +0530

    Added base html and base structure for the app

--- aboutme/main.py
+++ aboutme/main.py
@@ -3,8 +3,11 @@ from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
-def index():
-    return render_template("index.html")
+@application.route('/<username>/')
+def index(username=None):
+    if username:
+        return render_template("aboutme.html", page_title=username)
+    return render_template("index.html", page_title='About me')
 
 if __name__ == '__main__':
     application.run(debug=True, host="0.0.0.0", port=8888)
